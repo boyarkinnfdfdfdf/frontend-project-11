9665fe5513cfc143856925c1ac82b0f68824a860
Dimitry Plehanov <mitya-plekhanov@mail.ru>
2025-04-16 18:50:55 +0300

    4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './style.scss';
-console.log("2222222222222Hello from index.js");
 import 'bootstrap';
 import i18next from 'i18next';
 import resources from './locale/index.js';
@@ -8,9 +7,12 @@ import app from './watcher.js';
 const runApp = async () => {
   await i18next.init({
     lng: 'ru',
+    debug: true,
     resources,
   });
+
+  app();
 };
 
-app(runApp);
+runApp();
   
